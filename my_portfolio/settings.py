7e32b8178f0c7fd2814a6c65e17df76014e55dba
Chidex824 <chiderahwilliamsugwu@gmail.com>
2025-07-15 21:28:13 +0100

    my_portfolio

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-m16&7%-tt2lly(jz-35wy4=#prfu0!-^d(s6ca7$)$+bxq0qi4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
 
 
 # Application definition

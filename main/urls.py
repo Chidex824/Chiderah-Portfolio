875d0f137141ec1f2271acf2ca80f7c6b9c078cb
Chidex824 <chiderahwilliamsugwu@gmail.com>
2025-07-30 21:35:31 +0100

    main

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import PortfolioList
+from .views import index, PortfolioList
 
 urlpatterns = [
+    path('', index, name='index'),
     path('api/portfolio/', PortfolioList.as_view(), name='portfolio-list'),
 ]
--- main/views.py
+++ main/views.py
@@ -3,10 +3,24 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Portfolio
 from .serializers import PortfolioSerializer
+from django.views.generic import View
+from django.http import HttpResponse
+from django.conf import settings
+import os
 
 def index(request):
-    portfolios = Portfolio.objects.all()
-    return render(request, 'main/index.html', {'portfolios': portfolios})
+    return render(request, 'main/index.html')
+
+def serve_react_app(request):
+    """Serve React app for all non-API routes"""
+    try:
+        with open(os.path.join(settings.REACT_APP_DIR, 'index.html')) as f:
+            return HttpResponse(f.read())
+    except FileNotFoundError:
+        return HttpResponse(
+            "Please build the React app first. Run: cd frontend && npm run build",
+            status=501,
+        )
 
 class PortfolioList(APIView):
     def get(self, request):
